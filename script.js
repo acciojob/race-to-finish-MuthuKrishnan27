09b03b2c8aa910e44ef2ad3913d8dff5934efffa
MUTHUKRISHNAN S <77878932+MuthuKrishnan27@users.noreply.github.com>
2023-05-31 10:46:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
-window.promises = [];
+window.promises = Promise.any([prom1,prom2,prom3,prom4,prom5]);
 
+let prom1  = new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve("Prom1 resolved");
+	},2000);
+});
+
+let prom2  = new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve("Prom2 resolved");
+	},5000);
+});
+
+let prom3  = new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve("Prom3 resolved");
+	},1000);
+});
+
+let prom4  = new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve("Prom4 resolved");
+	},4000);
+});
+
+let prom5  = new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve("Prom5 resolved");
+	},3000);
+});
+
+let output = document.getElementById("output");
+output.innerText = window.promises;
 // Do not change the code above this
 // add your promises to the array `promises`
