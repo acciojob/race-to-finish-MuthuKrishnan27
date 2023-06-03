30be7ac1bcf0059e002b86636c65e778c6b0711e
MUTHUKRISHNAN S <77878932+MuthuKrishnan27@users.noreply.github.com>
2023-06-03 18:43:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 let prom1  = new Promise((resolve,reject)=>{
             setTimeout(()=>{
                 resolve("Prom1 resolved");
-            },2000);
+            },(Math.floor(Math.random() * 5) + 1)*1000;
         });
 
         let prom2  = new Promise((resolve,reject)=>{
             setTimeout(()=>{
                 resolve("Prom2 resolved");
-            },5000);
+            },(Math.floor(Math.random() * 5) + 1)*1000;
         });
 
         let prom3  = new Promise((resolve,reject)=>{
             setTimeout(()=>{
                 resolve("Prom3 resolved");
-            },1000);
+            },(Math.floor(Math.random() * 5) + 1)*1000;
         });
 
         let prom4  = new Promise((resolve,reject)=>{
             setTimeout(()=>{
                 resolve("Prom4 resolved");
-            },4000);
+            },(Math.floor(Math.random() * 5) + 1)*1000;
         });
 
         let prom5  = new Promise((resolve,reject)=>{
             setTimeout(()=>{
                 resolve("Prom5 resolved");
-            },3000);
+            },(Math.floor(Math.random() * 5) + 1)*1000;
         });
 
 window.promises = Promise.any([prom1,prom2,prom3,prom4,prom5]);
